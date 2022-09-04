a7121d70b8ed089218d47ce57f30f2b26c4f2cca
tugane <tooamiel@gmail.com>
2022-09-04 18:12:02 +0200

    Parsing Request Bodies

--- index.ts
+++ index.ts
@@ -1,4 +1,11 @@
 import express from "express";
 const app = express();
+import reminderRouter from "./routers/reminders";
+app.use(express.json());
+
+app.use("/reminders", reminderRouter);
+app.get("/", (req, res) => {
+  res.send("Hello world");
+});
 
 app.listen(8000, () => console.log("Server started"));
